bbd9fb95b4fce409325b853deb650d50336404f4
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-09-07 23:19:44 +0300

    Updates

---
+++ precache-manifest.d6321cc4d43537faac6cd22aa8e61ea7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ad5e78e3e8254647c3d011266a2072",
+    "url": "/HW16-ReactJS.Components/index.html"
+  },
+  {
+    "revision": "fd89549ff3bd5b987026",
+    "url": "/HW16-ReactJS.Components/static/css/main.549672d0.chunk.css"
+  },
+  {
+    "revision": "d179af44224670b19858",
+    "url": "/HW16-ReactJS.Components/static/js/2.419f12d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW16-ReactJS.Components/static/js/2.419f12d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd89549ff3bd5b987026",
+    "url": "/HW16-ReactJS.Components/static/js/main.5f0c2bf7.chunk.js"
+  },
+  {
+    "revision": "28055dfce240b3ca2f8e",
+    "url": "/HW16-ReactJS.Components/static/js/runtime-main.e06564e0.js"
+  },
+  {
+    "revision": "5c87994c964d115c484b909be298743b",
+    "url": "/HW16-ReactJS.Components/static/media/female.5c87994c.svg"
+  },
+  {
+    "revision": "075829956d01b4f9d072ca02aa84e2f2",
+    "url": "/HW16-ReactJS.Components/static/media/male.07582995.svg"
+  },
+  {
+    "revision": "2508df2055ae3e150ddc505e107f95e7",
+    "url": "/HW16-ReactJS.Components/static/media/unknown.2508df20.svg"
+  },
+  {
+    "revision": "e7875f017f6ec13298bbe3b818bbbd96",
+    "url": "/HW16-ReactJS.Components/static/media/user.e7875f01.svg"
+  }
+]);
